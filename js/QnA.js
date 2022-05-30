51be34839704e32351f6d03346b682e261e0a6fe
grinleaf <grinleaf774@gmail.com>
2022-05-30 19:06:33 +0900

    22.05.30 monday commit

--- js/QnA.js
+++ js/QnA.js
@@ -1,4 +1,4 @@
-let qnaArray1= [
+export let qnaArray1= [
     {q:"임시잠금 해제가 안돼요.",a:"안녕하세요. 따릉이입니다.<br><br>임시 잠금 해제가 안 풀리는 경우, 대부분 앱 설정 및 핸드폰 내의 프로그램에서 오류가 발생되는 경우가 대부분입니다.<br><br>아래 사항을 확인 후 재 실행 부탁드립니다.<br><br>1. 위치정보, 블루투스 활성화 여부 확인<br>2. 앱 종료 후, 로그인하여 앱 잠금 해제 <br>3. 핸드폰 종료 후, 앱 재 실행하여, 임시 잠금 해제 감사합니다."},
     {q:"대여하기 버튼을 눌렀으나, 대여 신청이 안돼요. (아이폰)",a:"※ 최초 대여신청을 했을때의 안내입니다.<br><br>계속 QR자전거가 대여되었는데, 특정자전거가 안될 경우 고장신고 부탁드립니다.<br><br>1. 앱의 경우, 현재 최신 버젼인지 먼저 확인을 합니다.<br><br>2. 앱 위치정보, 블루투스 관련 설정정보를 확인합니다.<br><br>위치 : 설정 > 개인정보보호> Bluetooth , 위치정보<br>- 위치정보, 블루투스가 설정되어 있어도, 설정요청 메시지가 나올 경우 따릉이 앱에 대한 위치정보 설정 필요 :  설정 > 앱목록 > 따릉이(참고)<br><br>* Bluetooth, 데이터 및 위치 서비스에 관하여<br>iOS 13 및 iPadOS 13에서 앱이 Bluetooth를 사용하려면 권한이 필요 없는 Bluetooth 기기로 오디오를 재생하는 경우를 제외하고 Bluetooth 기능 사용 권한을 요청해야 합니다.<br>설정 > 개인 정보 보호 > Bluetooth에서 기기의 Bluetooth 권한을 변경할 수 있습니다.<br><br>3. 연결되어 있는 블루투스 기기(스마트와치, 밴드, 이어폰 등)를 종료하고, 대여시도를 해봅니다.<br><br>4. 앱 내 실행되어 있는 앱(금융권, 카드사,증권사 등)을 종료하고 대여시도를 해봅니다.<br><br>5. 핸드폰을 종료하고 3~5초 정도 후, 다시 실행하여 자전거 대여시도를 해봅니다.<br><br>6. 네트워크 설정 초기화 확인 부탁드립니다. ( 설정 -> 일반 -> 재설정 -> 네트워크 재설정 )<br><br>7. 모두 해봤는데도 안 된다면, 시민의견 수렴에 부탁드립니다."},
     {q:"대여하기 버튼을 눌렀으나, 대여 신청이 안돼요.",a:"※ 최초 대여신청을 했을때의 안내입니다. <br><br>계속 QR자전거가 대여되었는데, 특정자전거가 안될 경우 고장신고 부탁드립니다.<br><br>1. 앱의 경우, 현재 최신 버젼인지 먼저 확인을 합니다.<br><br>2. 앱 위치정보, 블루투스 관련 설정정보를 확인합니다.<br><br>위치 : 설정 > 개인정보보호> Bluetooth , 위치정보<br>- 위치정보, 블루투스가 설정되어 있어도, 설정요청 메시지가 나올 경우 따릉이 앱에 대한 위치정보 설정 필요 :  설정 > 앱목록 > 따릉이(참고)<br><br>* Bluetooth, 데이터 및 위치 서비스에 관하여<br>iOS 13 및 iPadOS 13에서 앱이 Bluetooth를 사용하려면 권한이 필요 없는 Bluetooth 기기로 오디오를 재생하는 경우를 제외하고 Bluetooth 기능 사용 권한을 요청해야 합니다.<br>설정 > 개인 정보 보호 > Bluetooth에서 기기의 Bluetooth 권한을 변경할 수 있습니다.<br><br>3. 연결되어 있는 블루투스 기기(스마트와치, 밴드, 이어폰 등)를 종료하고, 대여시도를 해봅니다.<br><br>4. 앱 내 실행되어 있는 앱(금융권, 카드사,증권사 등)을 종료하고 대여시도를 해봅니다.<br><br>5. 핸드폰을 종료하고 3~5초 정도 후, 다시 실행하여 자전거 대여시도를 해봅니다.<br><br>6. 네트워크 설정 초기화 확인 부탁드립니다. ( 설정 -> 일반 -> 재설정 -> 네트워크 재설정 )<br><br>7. 절전모드 또는 V3 internet Security 에서 절전모드가 되어 있는지 확인합니다.<br><br>8. 모두 해봤는데도 안 된다면, 시민의견 수렴에 부탁드립니다."},
@@ -11,35 +11,78 @@ let qnaArray1= [
     {q:"자꾸 대여 실패가 발생합니다.",a:" ***따릉이 대여시 실패가 반복되신다면 다음 사항들을 확인 해 주십시오.***<br><br>1. 따릉이 앱 버전이 최신 버전인지 확인 해 주십시오. 최신 버전이 아니시라면 앱 삭제 후 스토어에서 최신 버전으로 설치 해 주세요. <br>* 바로가기만 지우시는 것이 아니라, 앱 자체를 지우셔야 합니다.<br>* 최신 버전이 아니라면 충돌이 발생할 가능성이 높아집니다. 꼭 최신 버전의 앱을 이용해 주세요.<br><br>2. 따릉이 앱 권한설정 확인 :    설정 > 애플리케이션 > 따릉이 > 권한 <br>* 위치, 저장공간, 전화, 카메라 권한 부여가 되어있는지 체크 해 주세요. 모든 권한이 허용되어 있어야 합니다.<br><br>3. 비행기 모드를 켰다가 끈 다음 위치와 블루투스 기능을 켜 주세요.<br><br>4. 휴대폰 네트워크(wifi, 블루투스, 기타 네트워크) 초기화 설정 > 일반 > 초기화 > 네트워크 설정 초기화<br>* 주의! ) 휴대폰에 저장된 wifi 비밀번호도 지워집니다. <br><br>5. 따릉이 앱 캐시 삭제 후 재실행<br>  안드로이드 : 설정 -> 애플리케이션 -> 따릉이 -> 저장공간 -> 데이터 삭제, 캐쉬 삭제<br>  아이폰 : 앱 -> 설정 -> 사파리 -> 방문기록 및 웹사이트 데이터 지우기<br><br>6. 갤럭시 스마트폰 최적화 프로그램 실행 : 갤럭시 스토어 -> Galaxy App Booster 실행으로 최적화(3~5분 소요)<br><br>7.  지도화면이 아닌 즐겨찾기 화면에서 따릉이 앱을 이용하시는 것을 추천 드립니다.<br>(1) 따릉이앱 - 메뉴 버튼 > 톱니바퀴 클릭 <br>(2) 메인화면 설정을 지도에서 즐겨찾기로 변경<br>(3) 텍스트 위주의 화면에서 내 위치 주변 탭을 클릭하여 원하는 대여소를 별표 클릭<br>(4) 해당 대여소 클릭후 자전거 대여 시도<br>* 지도화면에서 대여 시 메모리 점유율 및 저장용량의 영향이 있어 대여가 잘 안되실 수 있습니다. <br><br>8. 로그아웃, 재 로그인 후 재시도<br><br>9. 휴대폰 전원을 껐다 켜 보기<br>* 휴대폰을 주기적으로 재부팅 해 주시는 것은 앱 오류 가능성을 줄여 줄 수 있습니다. <br><br>10. 휴대폰에서 실행 중인 보안 프로그램들을 종료 후 재시도<br>* 휴대폰 내에 설치된 다른 어플(보안 프로그램, 금융권, 카드사, 증권사 앱 등)이 따릉이 앱의 정상 기능을 사용하지 못하도록 강제로 제어 할 수 있습니다. 해당 앱들을 종료하시고 대여를 시도해 주십시오.<br><br>11. 따릉이 앱이 백그라운드에서 돌아가게 될 경우 자동으로 로그인이 풀리는 등의 상황이  발생하여 자전거 대여시 제대로 연결되지 않는 경우가 있습니다. <br>* 앱 사용을 마치신 후 꼭 완전 종료를 해 주십시오. <br><br>12. 대여 실패시 다른 자전거로 대여 시도를 해 주십시오. <br>* 자전거(혹은 QR잠금장치/비콘 등)의 문제로 대여가 안될 수 있으므로 다른 자전거를 이용하여 주십시오.<br><br>13. 계속 QR자전거가 대여되었는데, 특정자전거가 안될 경우 고장신고 부탁드립니다. <br><br>14. 간헐적 시스템(통신/ 블루투스 연결) 오류가 발생할 수 있습니다. 이점은 시민 여러분들의 양해를 부탁 드립니다."}
 ];
 
-function qna_add(qnaArray){
-    let table= document.getElementById('qna_table')
-    for(var i=0; i<qnaArray.length; i++){
-        let tr= document.createElement('tr')
-        let qnaQImg= document.createElement('td')
-        let q1= document.createElement('img')
-        q1.src= "https://www.bikeseoul.com/img/faqlist_q.png"
-        qnaQImg.appendChild(q1)
-        let qnaQ= document.createElement('td')
-        qnaQ.appendChild(document.createTextNode(qnaArray[i].q))
-        qnaQ.onclick= click_q
-        let qnaAImg= document.createElement('td')
-        let q2=document.createElement('img')
-        q2.src= "https://www.bikeseoul.com/img/arr_open.png"
-        qnaAImg.appendChild(q2)
-        tr.appendChild(qnaQImg)
-        tr.appendChild(qnaQ)
-        tr.appendChild(qnaAImg)
-        table.appendChild(tr)
+function clickMenu(num){
+    switch(num){
+        case 0: document.getElementById('accordionExample01').style.display= 'block'
+        case 1: document.getElementById('accordionExample02').style.display= 'block'
+        case 2: document.getElementById('accordionExample03').style.display= 'block'
+        case 3: document.getElementById('accordionExample04').style.display= 'block'
+        case 4: document.getElementById('accordionExample04').style.display= 'block'
+        case 5: document.getElementById('accordionExample04').style.display= 'block'
+        case 6: document.getElementById('accordionExample04').style.display= 'block'
+        case 7: document.getElementById('accordionExample04').style.display= 'block'
     }
 }
 
-function click_q(){
+export default clickMenu
+
+// function qna_add(qnaArray){
+//     let title= document.getElementById('qna_q_title')
+//     let content= document.getElementById('qna_q_content')
+//     let num= title.innerHTML
+//     title.innerHTML= qnaArray[num].q
+//     content.innerHTML= qnaArray[num].a
+// }
+
+// function qna_switch(num){
     
-}
+// }
+
+// function qna_total(qnaArray){
+//     for(var i=0; i<qnaArray.length; i++){
+//     let total= document.getElementById('total')
+//     total.innerHTML="<div class='accordion-item'><h2 class='accordion-header' id='headingOne'>"+
+//         "<button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>"+
+//           "<span id='qna_q_title'>"+qnaArray[i].q+"</span>"+
+//         "</button></h2>"+
+//       "<div id='collapseOne' class='accordion-collapse collapse show' aria-labelledby='headingOne' data-bs-parent='#accordionExample'>"+
+//         "<div class='accordion-body'>"+
+//           "<span id='qna_q_content'>"+qnaArray[i].a+"</span>"+
+//         "</div></div></div></div>"
+//         qna_add(qnaArray1)
+//     }
+// }
+
+// function qna_add(qnaArray){
+//     let table= document.getElementById('qna_table')
+//     for(var i=0; i<qnaArray.length; i++){
+//         let tr= document.createElement('tr')
+//         let qnaQImg= document.createElement('td')
+//         let q1= document.createElement('img')
+//         q1.src= "https://www.bikeseoul.com/img/faqlist_q.png"
+//         qnaQImg.appendChild(q1)
+//         let qnaQ= document.createElement('td')
+//         qnaQ.appendChild(document.createTextNode(qnaArray[i].q))
+//         qnaQ.onclick= click_q
+//         let qnaAImg= document.createElement('td')
+//         let q2=document.createElement('img')
+//         q2.src= "https://www.bikeseoul.com/img/arr_open.png"
+//         qnaAImg.appendChild(q2)
+//         tr.appendChild(qnaQImg)
+//         tr.appendChild(qnaQ)
+//         tr.appendChild(qnaAImg)
+//         table.appendChild(tr)
+//     }
+// }
+
+// function click_q(){
+    
+// }
 
 // function clickMenu(){
 //     switch(){
-        qna_add(qnaArray1)
+        // qna_add_title(qnaArray1)
+        // qna_add(qnaArray1)
 //     }
     
 // }
