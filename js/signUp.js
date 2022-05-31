f3cf53576876525ade7a48039819381c69c781ed
Ina <inahpakkr@gmail.com>
2022-05-31 19:10:53 +0900

    집가자..

--- js/signUp.js
+++ js/signUp.js
@@ -15,131 +15,95 @@
         // 나이 종류에 대한 const 값 ( 새싹 따릉이 일 경우 , isAdult = false)
         const isAdult = false  // 디폴트 성인
 
+        // 폼에서 넘어오는 값들
+        var $name = $('userName').val()
+        var $userNum = $('#userNum').val()
+        var $phone = $('#userNum').val() // 이건 세개 조합해야 함
+        var $id = $('#id').val()
+        var $pw1 = $('#pw1').val()
+        var $pw2 = $('#pw2').val()
+        var $email = $('#userEmail').val()
+        var $domain = $('#userDomain').val()
+
+        $('#aa').click(
+            ()=>{ alert('눌리나 Test  1  ')}
+        )
 
         //----------------------------------------------
+        // 정규식 판단 메소드들
 
-            // 폼에서 넘어오는 값들
-            var $name = $('userName').val()
+        let vaildateEmail = (email , domain )=>{ //email :  사용자의 email값
 
+            const reEmailFirst = /\w+/    //  @ 앞
+            const reEmailSecond = /^[a-zA-Z0-9]+@[a-z]+.[a-z]+$/    // @ 뒤 
 
-            let vaildateEmail = (email , domain )=>{ //email :  사용자의 email값
+            return ((reEmailFirst.test(email)) && (reEmailSecond.test(domain))) ? true : false
 
-                const reEmailFirst = /\w+/    //  @ 앞
-                const reEmailSecond = /^[a-zA-Z0-9]+@[a-z]+.[a-z]+$/    // @ 뒤 
+        }
 
-                return ((reEmailFirst.test(email)) && (reEmailSecond.test(domain))) ? true : false
+        let vaildateId = id =>{ //id :  사용자의 id
+            const reId = /^\w{6,12}/     //영문, 숫자 6~12자리
+            
+            return reId.test(id)
+        }
 
-            }
+        let vaildateName = name =>{ //id :  사용자의 id
 
-            let vaildateId = id =>{ //id :  사용자의 id
+            // const reName = /^\w\w\w/    //17자 까지 허용 -> 정규식 다시.
 
-                const reId = /^\w{6,12}/     //영문, 숫자 6~12자리
-               
-                return reId.test(id)
+            
+            return alert(reName.test(name)) 
 
-            }
-            let vaildateName = name =>{ //id :  사용자의 id
+        }
 
-                const reName = /^\w{ ,17}/    //17자 까지 허용
+        let vaildatePw = pw =>{ //id :  사용자의 id
+            const rePw = /[a-z!@#$%^&*?~]{8,12}/      //영문, 숫자, 특수문자(!@#$%^&*?~)조합 8~12자리 
+            return rePw.test(pw)
+        }
+        //----------------------------------------------
 
-               
-                return reId.test(name)
-
-            }
-            let vaildatePw = pw =>{ //id :  사용자의 id
-
-                const rePw = /[a-z!@#$%^&*?~]{8,12}/      //영문, 숫자, 특수문자(!@#$%^&*?~)조합 8~12자리
-                
-                return rePw.test(pw)
-
-            }
-
-
+        // 등록 버튼을 누르면 폼 데이터가 검사 되기 전에, 유효성 검사를 위한 핸들러 ( 콜백 메소드)
+         $('#form-register').submit(function(e) {
+            e.preventDefault(); // 버튼을 눌러 넘어온 이벤트 객체임. 이 객체의 기본 동작을 막기. (안넘으면 넘어감)
+            var  $name = $('#userName').val()
+            // 여기서 사용자 정보를 가지고 전체적인 폼 검사흫 해야 함.
+           alert('제출')
+            alert($name)
+            vaildateName($name)
+    
+            
+        });
 
 
             
-        }
+        }) //JSQuery
+
+        $('#aa').click(
+            ()=>{ alert('눌리나 Test  2  ')}
+        )
   
 
 
-      //HTML 요소를 찾을 때 CSS 선택자를 이용해서 쉽게 선택할 수 있음.
-      // 문법 1. [JQuery('CSS 선택자' or 객체).원하는 기능]
-      // 문법 2. [$('CSS 선택자' or 객체).원하는 기능]
+        // 생성자 함수로 사용자 객체 만들기 - 더 생각해보기.
+        class SignUpUserInfo{
 
-      jQuery('#bbb').click( function(){ // 클릭돌 떼 이
-        alert('clicked')
-        $(this).hide
+            constructor(name, phoneNum, id, pw, email, cbEmail){
+                this.name = name,
+                this.phoneNum = phoneNum,
+                this.id = id, 
+                this.pw = pw, 
+                this.email = email,
+                this.cbEmail = cbEmail
+            }
 
+            toServer(){
+                // 포스트 방식으로 보내기 ㄱㄱ
+            }
+            fromServer(){
 
-      })
+            }
 
-      var $userNum = $('#userNum').val()
-      var $phone = $('#userNum').val() // 이건 세개 조합해야 함
-      var $id = $('#id').val()
-      var $pw = $('#pw').val()
-      var $email = $('#userNum').val()
-
-
-        //   var $btn= $('#ccc').click( isvail(name))
-
-
-      
-        function isvail( ,value){
-            alert(reName.test(value) )
-            
-            return reName.test(value)
-
-            
         }
-
-        // 등록 버튼을 누르면 폼 데이터가 검사 되기 전에, 유효성 검사를 위한 핸들러
-    $   ('#form-register').submit(function(e) {
-        e.preventDefault(); // 버튼을 눌러 넘어온 이벤트 객체임. 이 객체의 기본 동작을 막기. (안넘으면 넘어감)
-        
-        $(this).find('.txt-warning').empty().hide();
-
-        var email = $('#userName').next().html('테스트트트트트').show()
-
-        
-    });
-
-
-
-
-    })
-
-    // 등록 버튼을 누르면 폼 데이터가 검사 되기 전에, 유효성 검사를 위한 핸들러
-    $('#form-register').submit(function(e) {
-        e.preventDefault(); // 버튼을 눌러 넘어온 이벤트 객체임. 이 객체의 기본 동작을 막기. (안넘으면 넘어감)
-        
-        $(this).find('.txt-warning').empty().hide();
-
-        var email = $('#userName').next().html('테스트트트트트').show()
-
-        
-    });
-
-
-// 생성자 함수로 사용자 객체 만들기
-class SignUpUserInfo{
-
-    constructor(name, phoneNum, id, pw, email, cbEmail){
-        this.name = name,
-        this.phoneNum = phoneNum,
-        this.id = id, 
-        this.pw = pw, 
-        this.email = email,
-        this.cbEmail = cbEmail
-    }
-
-    toServer(){
-        // 포스트 방식으로 보내기 ㄱㄱ
-    }
-    fromServer(){
-
-    }
-
-}
 
 // 다음 버튼을 누르면 제이슨 객체로 만듬. 포스트로 보내야 하는데 ㅠ 나중에 ㄱ ㅜㅜ
 // function sub(name, phoneNum, id, pw, email, cbEmail){
