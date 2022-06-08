0cbc567cbb59dcb1b5edfe64b4f2fb0c4785384b
juhnny <iwibest@naver.com>
2022-06-08 19:10:05 +0900

    update index.html

--- indexEmailLogin.php
+++ indexEmailLogin.php
@@ -3,10 +3,32 @@
 
     $id = $_POST['id'];
     $pw = $_POST['pw'];
-    
+
+    $db = mysqli_connect("localhost", "kkpk", "nell##00", "kkpk");
+    mysqli_query($db, "set names utf8");
+    $query = "SELECT * FROM user_info WHERE id='$id' AND pw='$pw'";
+    $result = mysqli_query($db, $query);
 
     $arr = array();
-    $arr['name'] = "Kim";
+
+    if($result){
+        $rowCnt = mysqli_num_rows($result);
+        if($rowCnt==1){
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $arr['result'] = "loginSuccess";
+            $arr['id'] = $row['id'];
+            $arr['phone'] = $row['phoneNum'];
+            $arr['time'] = 419;
+            $arr['distance'] = 62.23;
+            $arr['calorie'] = 1823.58;
+            $arr['carbonReduced'] = 14.45;
+        }else{
+            $arr['result'] = "loginFailure";
+        }
+    } else {
+        $arr['result'] = "databaseError";
+    }
+
     echo json_encode($arr);
     // echo '{"name":"Sam", "age": 16}'
 ?>
